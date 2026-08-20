9ee630912a9fb9c73c3fd677b2763bc2a5f67aa9
Vince <vincepalaming2108@gmail.com>
2026-08-20 14:49:04 +0800

    CORSSSSSSS

--- config/cors.php
+++ config/cors.php
@@ -9,7 +9,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://vnc-clothing-beryl.vercel.app/'],
+    'allowed_origins' => ['https://vnc-clothing-beryl.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
